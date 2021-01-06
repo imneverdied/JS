5d07a5162613e0cf41f3f98aec0b0aafb2ab9e28
imneverdied <imneverdied@gmail.com>
2021-01-06 13:33:02 +0800

    Create AAA.js

---
+++ AAA.js
@@ -0,0 +1,1 @@
+function changeColor() { var elem = document.getElementById('para'); var newColor=elem.style.color; if(newColor=='blue') {newColor='red' } else {newColor='blue'} elem.style.color = newColor; } function cColor(value) { var elem = document.getElementById('para'); elem.style.color = value; elem.innerHTML=value; }
