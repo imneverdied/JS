76dedea0b07aa4456793dafb360869af6bbdb985
imneverdied <imneverdied@gmail.com>
2024-09-10 11:01:25 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+[
+  {
+    "id": 1,
+    "title": "好吃的牛肉麵",
+    "area": "中壢區",
+    "tags": ["美食", "小吃"],
+    "embedUrl": "https://www.instagram.com/reel/C8l0nk1ySYr/?utm_source=ig_embed&amp;utm_campaign=loading",
+    "address": "桃園市中壢區中央西路二段30號",
+    "website": "https://www.instagram.com/reel/C8l0nk1ySYr/?utm_source=ig_web_copy_link",
+    "description": "這家牛肉麵店的湯頭濃郁，牛肉軟嫩多汁，是中壢區的人氣美食。"
+  },
+  {
+    "id": 2,
+    "title": "桃園神社",
+    "area": "桃園區",
+    "tags": ["旅遊景點"],
+    "embedUrl": "https://www.instagram.com/reel/C9e7Ql1yYSC/?utm_source=ig_embed&amp;utm_campaign=loading",
+    "address": "桃園市桃園區中正路178號",
+    "website": "https://www.instagram.com/reel/C9e7Ql1yYSC/?utm_source=ig_web_copy_link",
+    "description": "桃園神社是一座保存完好的日式神社，環境幽靜，是拍照打卡的熱門景點。"
+  }
+]
